a33527ee76dc6758c38d7b482233a2531ba9d67b
sszczure <114861956+sszczure@users.noreply.github.com>
2022-10-02 15:14:20 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/Application.java
@@ -0,0 +1,10 @@
+package pl.edu.wszib;
+
+
+
+public class Application {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass("1","2","3","4");
+
+    }
+}
---
+++ src/main/java/pl/edu/wszib/MyClass.java
@@ -0,0 +1,15 @@
+package pl.edu.wszib;
+
+public class MyClass {
+    private String field1;
+     String field2;
+    protected String field3;
+    public String field4;
+
+    public MyClass(String field1, String field2, String field3, String field4) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+        this.field4 = field4;
+    }
+}
